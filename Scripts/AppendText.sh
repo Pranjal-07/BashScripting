b980a443bb6dc90342c776d537d63544be3f2f45
Pranjal <sureshnagal7890@gmail.com>
2022-09-03 00:31:06 +0530

    Scripts

---
+++ Scripts/AppendText.sh
@@ -0,0 +1,16 @@
+# Appending output to the end of a text file.
+echo -e "Enter the name of the file: \c"
+read file_name
+
+if [ -f $file_name ]
+then
+    if [ -w $file_name ]
+    then
+        echo "Type some data. To eit typing press CTRL+d"
+        cat >> $file_name
+    else
+        echo "$file_name does not have write permissions."
+    fi
+else
+    echo "File $file_name doesn't exist"
+fi
